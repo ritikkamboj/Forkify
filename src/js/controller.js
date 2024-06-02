a1673b9a555079003882d9164b47695bed4f3cb4
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-06-02 23:10:12 +0530

    makes the recipe object property more handy to use

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,26 +16,37 @@ console.log("jai maata di ")
 
 const dummy = async function () {
   try {
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);  // here this url is for specific id as we can see also
 
     const data = await res.json();
     console.log(res);
-    if(!res.ok){
+    if (!res.ok) {
       throw new Error(`${data.message}, ${res.status}`)
     }
-
-   
-
     console.log(data);
+    console.log(data.data);
+    console.log(data.data.recipe);
 
+    let recipe = data.data.recipe;
+    // let {recipe} = data.data;   this is the destructuring way 
 
+    
+    recipe ={
+      id :recipe.id,
+      title:recipe.title,
+      publisher:recipe.publisher,
+      sourceUrl:recipe.source_url,
+      image:recipe.image_url,
+      servings:recipe.servings,
+      cookingTimes:recipe.cooking_time,
+      ingredients:recipe.ingredients
 
-
+    };
+    console.log(recipe);
 
   } catch (err) {
     console.log(`${err.message} yeh apne waala error hai`);
   }
 
 }
-
 dummy();
