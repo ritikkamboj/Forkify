e727b68d4c3f897c16a39e72eb4c0e2ee1bc472c
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-06-03 17:16:52 +0530

    inserted the dynamic HTML in our DOM for .recipe container

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,8 @@
+import icons from "url:../img/icons.svg";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+console.log(icons)
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -12,11 +17,25 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 console.log("jai maata di ")
+
+const renderSpinner = function (parentEl) {
+  const html = `<div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div>`;
+  parentEl.innerHTML='';
+  parentEl.insertAdjacentHTML('afterbegin', html);
+}
 // code to fetch the the data form JS api by using sync and awist and 
 
 const dummy = async function () {
   try {
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);  // here this url is for specific id as we can see also
+    renderSpinner(recipeContainer)
+
+
+    // const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);  // here this url is for specific id as we can see also
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e88b9`);  // here this url is for specific id as we can see also
 
     const data = await res.json();
     console.log(res);
@@ -30,19 +49,111 @@ const dummy = async function () {
     let recipe = data.data.recipe;
     // let {recipe} = data.data;   this is the destructuring way 
 
-    
-    recipe ={
-      id :recipe.id,
-      title:recipe.title,
-      publisher:recipe.publisher,
-      sourceUrl:recipe.source_url,
-      image:recipe.image_url,
-      servings:recipe.servings,
-      cookingTimes:recipe.cooking_time,
-      ingredients:recipe.ingredients
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
 
     };
     console.log(recipe);
+
+    const markup = `
+    <figure class="recipe__fig">
+    <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${recipe.title}</span>
+    </h1>
+  </figure>
+
+  <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+      <span class="recipe__info-text">servings</span>
+
+      <div class="recipe__info-buttons">
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-minus-circle"></use>
+          </svg>
+        </button>
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="${icons}#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="${icons}#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+  </div>
+
+  <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+      ${recipe.ingredients.map(ing => {
+      return `<li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>`}).join('')}
+
+      
+    </ul>
+  </div>
+
+  <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${recipe.sourceUrl}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </a>
+  </div>
+  `;
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+
+
 
   } catch (err) {
     console.log(`${err.message} yeh apne waala error hai`);
@@ -50,3 +161,4 @@ const dummy = async function () {
 
 }
 dummy();
+
